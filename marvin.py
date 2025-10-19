3bdfd9f1989c00a8979efaa3a27d2f90ffe8abfc
Martin Scheele <martin.scheele@proton.me>
2025-10-18 20:10:32 -0400

    new instructions, opcode format; minor adjustments

--- marvin.py
+++ marvin.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from datetime import date
 import os
 import random
 import struct
@@ -18,14 +19,27 @@ the emulator prints the assembled instructions to stdout before simulating them.
 
 # Maps opcodes to their binary 8-bit codes.
 opcode_to_bin = {
-    "halt":  0b00000000, "read":   0b00000001, "write": 0b00000010, "nop":    0b00000011,
-    "set0":  0b00000100, "set1":   0b00000101, "setn":  0b00000110, "addn":   0b00000111,
-    "copy":  0b00001000, "neg":    0b00001001, "add":   0b00001010, "sub":    0b00001011,
-    "mul":   0b00001100, "div":    0b00001101, "mod":   0b00001110, "jumpn":  0b00001111,
-    "jumpr": 0b00010000, "jeqzn":  0b00010001, "jnezn": 0b00010010, "jgen":   0b00010011,
-    "jlen":  0b00010110, "jeqn":   0b00010100, "jnen":  0b00010101, "jgtn":   0b00010111,
-    "jltn":  0b00011000, "calln":  0b00011001, "pushr": 0b00011010, "popr":   0b00011011,
-    "loadn": 0b00011100, "storen": 0b00011101, "loadr": 0b00011110, "storer": 0b00011111
+    # system instructions
+    "halt":   0b00000000, "readi":  0b00000001, "readf":  0b00000010, "readc":  0b00000011,
+    "writei": 0b00000100, "writef": 0b00000101, "writec": 0b00000110, "seed":   0b00000111,
+    "rand":   0b00001000, "time":   0b00001001, "date":   0b00001010, "nop":    0b00001111,
+    # arithmetic instructions
+    "neg":    0b00010000, "add":    0b00010001, "sub":    0b00010010, "mul":    0b00010011,
+    "div":    0b00010100, "mod":    0b00010101, "fneg":   0b00010110, "fadd":   0b00010111,
+    "fsub":   0b00011000, "fmul":   0b00011001, "fdiv":   0b00011010,
+    # bitwise instructions
+    "and":    0b00100000, "or":     0b00100001, "xor":    0b00100010, "not":    0b00100011,
+    "lshl":   0b00100100, "lshr":   0b00100101, "ashl":   0b00100110, "ashr":   0b00100111,
+    # jump instructions
+    "jumpn":  0b00110000, "jumpr":  0b00110001, "jeqzn":  0b00110010, "jnezn":  0b00110011,
+    "jgen":   0b00110100, "jlen":   0b00110101, "jeqn":   0b00110110, "jnen":   0b00110111,
+    "jgtn":   0b00111000, "jltn":   0b00111001, "calln":  0b00111010,
+    # register instructions
+    "setn":   0b01000000, "addn":   0b01000001, "setf":   0b01000010, "addf":   0b01000011,
+    "copy":   0b01000100,
+    # memory instructions
+    "pushr":  0b01010000, "popr":   0b01010001, "loadn":  0b01010010, "storen": 0b01010011,
+    "loadr":  0b01010100, "storer": 0b01010101,
 }
 
 # Maps 8-bit binary codes to the opcodes they represent.
@@ -34,10 +48,15 @@ bin_to_opcode = {opcode_to_bin[opcode]: opcode for opcode in opcode_to_bin.keys(
 # Maps register names to their binary 4-bit codes.
 reg_to_bin = {
     "r0":  0b0000, "r1":  0b0001, "r2":  0b0010, "r3":  0b0011, 
+    "a0":  0b0000, "a1":  0b0001, "a2":  0b0010, "a3":  0b0011, 
+
     "r4":  0b0100, "r5":  0b0101, "r6":  0b0110, "r7":  0b0111, 
+
     "r8":  0b1000, "r9":  0b1001, "r10": 0b1010, "r11": 0b1011,
+                                                 "ra":  0b1011,
+
     "r12": 0b1100, "r13": 0b1101, "r14": 0b1110, "r15": 0b1111,
-    "ra":  0b1100, "rv":  0b1101, "fp":  0b1110, "sp":  0b1111
+    "rv":  0b1100, "fp":  0b1101, "sp":  0b1110, "gp":  0b1111
 }
 
 def main():
@@ -84,68 +103,66 @@ def main():
         expectedID += 1
 
         # Validate the instruction arguments.
+        # TODO: validate new instructions
         id, opcode, args = int(toks[0]), toks[1], toks[2:]
         if opcode in {"halt", "nop"}:
             if len(args) != 0:
                 sys.exit(f"Error {inFile}@{lineno}: '{opcode}' expects 0 arguments")
-            tuples.append((lineno, id, opcode))
-        elif opcode in {"jumpr", "read", "set0", "set1", "write"}:
+        elif opcode in {"jumpr", "readi", "set0", "set1", "writei"}:
             if len(args) != 1:
                 sys.exit(f"Error {inFile}@{lineno}: '{opcode}' expects 1 argument, rX")
-            if not validReg(toks[2]):
+            if not valid_reg(toks[2]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[2]}'")
-            tuples.append((lineno, id, opcode, toks[2]))
         elif opcode in {"jumpn"}:
             if len(args) != 1:
                 sys.exit(f"Error {inFile}@{lineno}: '{opcode}' expects 1 argument, N")
             if not isNum(toks[2]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid number '{toks[2]}'")
-            tuples.append((lineno, id, opcode, toks[2]))
         elif opcode in {"copy", "loadr", "neg", "popr", "pushr", "storer"}:
             if len(args) != 2:
                 sys.exit(f"Error {inFile}@{lineno}: '{toks[1]}' expects 2 arguments, rX rY")
-            if not validReg(toks[2]):
+            if not valid_reg(toks[2]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[2]}'")
-            if not validReg(toks[3]):
+            if not valid_reg(toks[3]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[3]}'")
-            tuples.append((lineno, id, opcode, toks[2], toks[3]))
         elif opcode in {"addn", "calln", "jeqzn", "jnezn", "setn"}:
             if len(args) != 2:
                 sys.exit(f"Error {inFile}@{lineno}: '{toks[1]}' expects 2 arguments, rX N")
-            if not validReg(toks[2]):
+            if not valid_reg(toks[2]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[2]}'")
             if not isNum(toks[3]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid number '{toks[3]}'")
-            tuples.append((lineno, id, opcode, toks[2], toks[3]))
         elif opcode in {"add", "div", "mod", "mul", "sub"}:
             if len(args) != 3:
                 sys.exit(f"Error {inFile}@{lineno}: '{toks[1]}' expects 3 arguments, rX rY rZ")
-            if not validReg(toks[2]):
+            if not valid_reg(toks[2]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[2]}'")
-            if not validReg(toks[3]):
+            if not valid_reg(toks[3]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[3]}'")
-            if not validReg(toks[4]):
+            if not valid_reg(toks[4]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[4]}'")
-            tuples.append((lineno, id, opcode, toks[2], toks[3], toks[4]))
         elif opcode in {"jeqn", "jgen", "jgtn", "jlen", "jltn", "jnen", "loadn", "storen"}:
             if len(args) != 3:
                 sys.exit(f"Error {inFile}@{lineno}: '{toks[1]}' expects 3 arguments, rX rY N")
-            if not validReg(toks[2]):
+            if not valid_reg(toks[2]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[2]}'")
-            if not validReg(toks[3]):
+            if not valid_reg(toks[3]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid register '{toks[3]}'")
             if not isNum(toks[4]):
                 sys.exit(f"Error {inFile}@{lineno}: invalid number '{toks[4]}'")
-            tuples.append((lineno, id, opcode, toks[2], toks[3], toks[4]))
+        else:
+            sys.exit(f"Error {inFile}@{lineno}: invalid instructions '{opcode}'")
+        tuples.append((lineno, id, opcode, *toks[2:]))
 
     # Additional validation of instruction arguments.
+    # TODO: additional validation for new instructions
     for t in tuples:
         lineno, id, opcode, args = t[0], t[1], t[2], t[3:]
         if opcode in {"jumpn"} and int(args[0]) >= len(tuples):
             sys.exit(f"Error {inFile}@{lineno}: invalid instruction address '{args[0]}'")
-        elif opcode in {"addn", "setn"} and not validNum(int(args[1])):
+        elif opcode in {"addn", "setn"} and not valid_int(int(args[1])):
             sys.exit(f"Error {inFile}@{lineno}: invalid number '{args[1]}'")
-        elif opcode in {"loadn", "storen"} and not validNum(int(args[2])):
+        elif opcode in {"loadn", "storen"} and not valid_int(int(args[2])):
             sys.exit(f"Error {inFile}@{lineno}: invalid address '{args[2]}'")
         elif opcode in {"calln", "jeqzn", "jnezn"} and int(args[1]) >= len(tuples):
             sys.exit(f"Error {inFile}@{lineno}: invalid instruction address '{args[1]}'")
@@ -169,9 +186,10 @@ def assemble(tuples: list[tuple[int, int, str, *tuple[str, ...]]], verbose: bool
         id, opcode, args = t[1], t[2], t[3:]
         bArg1, bArg2, bArg3 = 0, 0, 0
 
+        # TODO: assemble new instructions
         if opcode in {"halt", "nop"}:
             bArg1, bArg2, bArg3 = 0, 0, 0
-        elif opcode in {"jumpr", "read", "set0", "set1", "write"}:
+        elif opcode in {"jumpr", "readi", "set0", "set1", "writei"}:
             bArg1, bArg2, bArg3 = 0, 0, reg_to_bin[args[0]]
         elif opcode in {"jumpn"}:
             val = int(args[0])
@@ -256,47 +274,86 @@ def simulate(machineCodes: list[int], debug: bool):
         if debug:
             debug_exec(code)
 
-        # Simulation the instruction given by opcode.
+        # Simulate the instruction given by opcode.
         instructions[opcode](code)
 
 def debug_exec(code: int):
     global reg, mem, pc
     opcode = bin_to_opcode[code >> 24]
-    # TODO: how to represent different types?
     print(f"pc: {pc} opcode: {opcode}")
     print(f"r0:  {tc_b32_to_int(reg[0]): 10d} r1:  {tc_b32_to_int(reg[1]): 10d} r2:  {tc_b32_to_int(reg[2]): 10d} r3:  {tc_b32_to_int(reg[3]): 10d}")
     print(f"r4:  {tc_b32_to_int(reg[4]): 10d} r5:  {tc_b32_to_int(reg[5]): 10d} r6:  {tc_b32_to_int(reg[6]): 10d} r7:  {tc_b32_to_int(reg[7]): 10d}")
     print(f"r8:  {tc_b32_to_int(reg[8]): 10d} r9:  {tc_b32_to_int(reg[9]): 10d} r10: {tc_b32_to_int(reg[10]): 10d} r11: {tc_b32_to_int(reg[11]): 10d}")
     print(f"r12: {tc_b32_to_int(reg[12]): 10d} r13: {tc_b32_to_int(reg[13]): 10d} r14: {tc_b32_to_int(reg[14]): 10d} r15: {tc_b32_to_int(reg[15]): 10d}")
     for i in range(65535, tc_b32_to_int(reg[15]) - 1, -1):
-        if i == reg[14]:
+        if i == reg[13]:
             print("*", end="")
-        if i == reg[15]:
+        if i == reg[14]:
             print("^", end="")
         print(mem[i])
     _ = input()
 
-def op_halt(code: int):
+# System Instructions
+
+def op_halt(_: int):
     sys.exit()
 
-def op_read(code: int):
+def op_readi(code: int):
     global reg, pc
     arg1 = code & 0xf
     while True:
         try:
             x = int(input())
-            if (validNum(x)):
+            if (valid_int(x)):
                 break
             raise ValueError
         except ValueError:
-            print("Illegal input: number must be in [-32768, 32767]")
+            print("Illegal input: input must be a number must be in [-32768, 32767]")
     reg[arg1] = tc_int_to_b32(x)
     pc += 1
 
-def op_write(code: int):
+def op_readf(code: int):
+    global reg, pc
+    arg1 = code & 0xf
+    while True:
+        try:
+            x = float(input())
+            break
+        except ValueError:
+            # TODO: better error message
+            print("Illegal input: input must be a number")
+    reg[arg1] = fp_float_to_f32(x)
+    pc += 1
+
+def op_readc(code: int):
+    global reg, pc
+    arg1 = code & 0xf
+    while True:
+        try:
+            x = sys.stdin.read(1)
+            break
+        except Exception:
+            # TODO: are there edge cases here?
+            pass
+    reg[arg1] = ord(x)
+    pc += 1
+
+def op_writei(code: int):
     global reg, pc
     arg1 = code & 0xf
     print(tc_b32_to_int(reg[arg1]))
+    pc += 1
+
+def op_writef(code: int):
+    global reg, pc
+    arg1 = code & 0xf
+    print(fp_f32_to_float(reg[arg1]))
+    pc += 1
+
+def op_writec(code: int):
+    global reg, pc
+    arg1 = code & 0xf
+    print(chr(reg[arg1]))
     pc += 1
 
 def op_seed(code: int):
@@ -313,13 +370,28 @@ def op_rand(code: int):
     lo = tc_b32_to_int(reg[arg1])
     hi = tc_b32_to_int(reg[arg2])
     reg[arg3] = tc_int_to_b32(random.randint(lo, hi))
+    pc += 1
 
 def op_time(code: int):
-    raise NotImplementedError
+    global reg, pc
+    arg1 = code & 0xf
+    now = time.localtime()
+    reg[arg1] = now.tm_hour * 3600 + now.tm_min * 60 + now.tm_sec
+    pc += 1
 
-def op_nop(code: int):
+
+def op_date(code: int):
+    global reg, pc
+    arg1 = code & 0xf
+    today = date.today()
+    reg[arg1] = today.year << 19 | today.month << 9 | today.day
+    pc += 1
+
+def op_nop(_: int):
     global pc
     pc += 1
+
+# Arithmetic instructions
 
 def op_neg(code: int):
     global reg, pc
@@ -367,6 +439,116 @@ def op_mod(code: int):
     arg3 = code & 0xf
     reg[arg1] = reg[arg2] % reg[arg3]
     pc += 1
+
+def op_fneg(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 4) >> 4
+    arg2 = (code & 0xf)
+    reg[arg1] = reg[arg2] ^ (1 << 31)
+    pc += 1
+
+def op_fadd(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = fp_float_to_f32(fp_f32_to_float(reg[arg2]) + fp_f32_to_float(reg[arg3]))
+    pc += 1
+
+def op_fsub(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = fp_float_to_f32(fp_f32_to_float(reg[arg2]) - fp_f32_to_float(reg[arg3]))
+    pc += 1
+
+def op_fmul(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = fp_float_to_f32(fp_f32_to_float(reg[arg2]) * fp_f32_to_float(reg[arg3]))
+    pc += 1
+
+def op_fdiv(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = fp_float_to_f32(fp_f32_to_float(reg[arg2]) / fp_f32_to_float(reg[arg3]))
+    pc += 1
+
+# Bitwise instructions
+
+def op_and(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = reg[arg2] & reg[arg3]
+    pc += 1
+
+def op_or(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = reg[arg2] | reg[arg3]
+    pc += 1
+
+def op_xor(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = reg[arg2] ^ reg[arg3]
+    pc += 1
+
+def op_not(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 4) >> 4
+    arg2 = (code & 0xf)
+    reg[arg1] = ~reg[arg2]
+    pc += 1
+
+def op_lshl(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = (reg[arg2] << reg[arg3]) & 0xffffffff
+    pc += 1
+
+def op_lshr(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    reg[arg1] = (reg[arg2] >> reg[arg3]) & 0xffffffff
+    pc += 1
+
+def op_ashl(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    sign = reg[arg2] & (1 << 31)
+    temp = ((reg[arg2] ^ sign) << reg[arg3]) & 0xffffffff
+    reg[arg1] = 0 if temp == 0 else temp | sign
+    pc += 1
+
+def op_ashr(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 8) >> 8
+    arg2 = (code & 0xf << 4) >> 4
+    arg3 = (code & 0xf)
+    sign = reg[arg2] & (1 << 31)
+    temp = ((reg[arg2] ^ sign) >> reg[arg3]) & 0xffffffff
+    reg[arg1] = 0 if temp == 0 else temp | sign
+    pc += 1
+
+# Jump Instructions
 
 def op_jumpn(code: int):
     global pc
@@ -440,17 +622,7 @@ def op_calln(code: int):
     reg[arg1] = pc + 1
     pc = arg2
 
-def op_set0(code: int):
-    global reg, pc
-    arg1 = code & 0xf
-    reg[arg1] = 0
-    pc += 1
-
-def op_set1(code: int):
-    global reg, pc
-    arg1 = code & 0xf
-    reg[arg1] = 1
-    pc += 1
+# Register instructions
 
 def op_setn(code: int):
     global reg, pc
@@ -464,6 +636,20 @@ def op_addn(code: int):
     arg1 = (code & 0xf << 16) >> 16
     arg2 = code & 0xffff
     reg[arg1] = tc_add(reg[arg1], tc_b16_to_b32(arg2))
+    pc += 1
+
+def op_setf(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 16) >> 16 
+    arg2 = code & 0xffff
+    reg[arg1] = fp_f16_to_f32(arg2)
+    pc += 1
+
+def op_addf(code: int):
+    global reg, pc
+    arg1 = (code & 0xf << 16) >> 16
+    arg2 = code & 0xffff
+    reg[arg1] = fp_float_to_f32(fp_f32_to_float(reg[arg1]) + fp_f32_to_float(reg[arg2]))
     pc += 1
 
 def op_copy(code: int):
@@ -529,6 +715,13 @@ instructions = {}
 for op in opcode_to_bin.keys():
     func = "op_" + op
     instructions[op] = globals()[func] if func in globals() else op_unimp
+
+def extract_args(code: int, *args: tuple[int, int]) -> list[int]:
+    ret: list[int] = [] 
+    for arg in args:
+        mask, bit = arg
+        ret.append((code & mask << bit) >> bit)
+    return ret
 
 def tc_int_to_b16(val: int) -> int:
     if val < 0:
@@ -638,11 +831,11 @@ def isNum(s: str) -> bool:
     return ans
 
 # Return True if n is a valid signed 16-bit integer, and False otherwise.
-def validNum(n: int) -> bool:
+def valid_int(n: int) -> bool:
     return -2 ** 15 <= n <= 2 ** 15 - 1
 
 # Return True if s is "r" followed by a number from the interval [0, 15], and False otherwise.
-def validReg(s: str) -> bool:
+def valid_reg(s: str) -> bool:
     return s in reg_to_bin.keys()
 
 if __name__ == "__main__":
